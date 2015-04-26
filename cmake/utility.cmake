2d06f952e0368e21f3398c69d2eb0225f55e84d6
Kataev <Kataev@c90b6321-6eb1-4274-a4bb-7129d0cd3ae9>
2015-04-26 16:53:10 +0000

    [TSAR-cmake] Disable RTTI in TSAR if it is disabled in LLVM (by default), add support for LLVM version dependent patch.
    Put llvm-[version].cmake module in 'patches' directory to add LLVM version dependent path (example llvm-3.4.2.patch).
    
    git-svn-id: http://svn.dvm-system.org/svn/dvmhrepo@2549 c90b6321-6eb1-4274-a4bb-7129d0cd3ae9

---
+++ cmake/utility.cmake
@@ -0,0 +1,18 @@
+# Replaces a compiler option or switch `old' in `var' by `new'.
+# If `old' is not in `var', appends `new' to `var'.
+# Example: llvm_replace_compiler_option(CMAKE_CXX_FLAGS_RELEASE "-O3" "-O2")
+# If the option already is on the variable, don't add it:
+# This code is taken from LLVM (cmake/modules/LLVMProcessSources.cmake).
+function(sapfor_replace_compiler_option var old new)
+    if("${${var}}" MATCHES "(^| )${new}($| )")
+        set(n "")
+    else()
+        set(n "${new}")
+    endif()
+        if("${${var}}" MATCHES "(^| )${old}($| )")
+        string(REGEX REPLACE "(^| )${old}($| )" " ${n} " ${var} "${${var}}")
+    else()
+        set(${var} "${${var}} ${n}")
+    endif()
+    set( ${var} "${${var}}" PARENT_SCOPE )
+endfunction(sapfor_replace_compiler_option)
