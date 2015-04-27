79fc510eaca3cec009a5045313e3bb512c5800d0
Kataev <Kataev@c90b6321-6eb1-4274-a4bb-7129d0cd3ae9>
2015-04-27 12:49:21 +0000

    [TSAR-cmake] Fix: enable C++ 11 support in GCC compatible compilers.
    git-svn-id: http://svn.dvm-system.org/svn/dvmhrepo@2558 c90b6321-6eb1-4274-a4bb-7129d0cd3ae9

--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -1,7 +1,7 @@
 # Replaces a compiler option or switch `old' in `var' by `new'.
 # If `old' is not in `var', appends `new' to `var'.
 # Example: llvm_replace_compiler_option(CMAKE_CXX_FLAGS_RELEASE "-O3" "-O2")
-# If the option already is on the variable, don't add it:
+# If the option already is on the variable, don't add it.
 # This code is taken from LLVM (cmake/modules/LLVMProcessSources.cmake).
 function(sapfor_replace_compiler_option var old new)
     if("${${var}}" MATCHES "(^| )${new}($| )")
@@ -16,3 +16,12 @@ function(sapfor_replace_compiler_option var old new)
     endif()
     set( ${var} "${${var}}" PARENT_SCOPE )
 endfunction(sapfor_replace_compiler_option)
+
+# Appends 'value' to a list of specified variables.
+# Example: append("-std=gnu++11" CMAKE_CXX_FLAGS)
+# This code is taken from LLVM (cmake/modulesHandleLLVMOptions.cmake).
+function(append value)
+  foreach(variable ${ARGN})
+    set(${variable} "${${variable}} ${value}" PARENT_SCOPE)
+  endforeach(variable)
+endfunction()
