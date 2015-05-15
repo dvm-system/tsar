467fccb8e5240b327dbefff132f585d7ff68682c
Kataev <Kataev@c90b6321-6eb1-4274-a4bb-7129d0cd3ae9>
2015-05-15 11:36:52 +0000

    [TSAR] Fix functions to search back edges in a directed graph.
    git-svn-id: http://svn.dvm-system.org/svn/dvmhrepo@2655 c90b6321-6eb1-4274-a4bb-7129d0cd3ae9

--- src/tsar_graph.h
+++ src/tsar_graph.h
@@ -33,6 +33,7 @@ enum DFSColor {
 /// Color of a node which breadth-first search (BFS) has visited.
 typedef DFSColor BFSColor;
 
+namespace detail {
 /// \brief Returns first back edge in the specified directed graph.
 ///
 /// This function implements a preorder traversal using DFS and calculates
@@ -40,45 +41,81 @@ typedef DFSColor BFSColor;
 /// assuming that nodes from the specified list have been already visited and
 /// colored.
 /// \param [in] N Node that must be visited.
-/// \param [in, out] VisitedNodes Already visited nodes.
-/// \return Back edge represented as a pair of two nodes: successor and predecessor.
+/// \param [in, out] VisitedNodes Already visited nodes and the new node N 
+/// that must be visited.
+/// \return Back edge represented as a pair of two nodes: successor and
+/// predecessor.
 /// If back edge in not found this function returns a pair of nullptrs
+/// \pre The llvm::GraphTraits class should be specialized by GraphType.
+/// Note that GraphType is generally a pointer type, for example BasicBlock *.
+/// The new node N that must be visited should be located in the VisitedNodes 
+/// collection.
+/// \note It is not possible to use generic depth first graph iterator
+/// which is implemented in LLVM, because this implementation does not collect
+/// information about color of the node. So we can not determine whether all
+/// children of some node have been already visited.
 template<class GraphType>
 std::pair<
-  typename llvm::GraphTraits<GraphType *>::NodeType *,
-  typename llvm::GraphTraits<GraphType *>::NodeType *> findBackEdge(
-    typename llvm::GraphTraits<GraphType *>::NodeType *N,
-    llvm::DenseMap<typename llvm::GraphTraits<GraphType *>::NodeType *, DFSColor>
+  typename llvm::GraphTraits<GraphType>::NodeType *,
+  typename llvm::GraphTraits<GraphType>::NodeType *> findBackEdge(
+    typename llvm::GraphTraits<GraphType>::NodeType *N,
+    llvm::DenseMap<typename llvm::GraphTraits<GraphType>::NodeType *, DFSColor>
     &VisitedNodes) {
-  typedef llvm::GraphTraits<GraphType *> GT;
+  typedef llvm::GraphTraits<GraphType> GT;
   typedef typename GT::NodeType NodeType;
-  assert(N && "Visited node must not be null!");
-  VisitedNodes.insert(std::make_pair(N, COLOR_GRAY));
+  assert(N && "Node must not be null!");
+  assert(VisitedNodes.count(N) && "Node must be located in the VisitedNodes collection!");
   for (auto CI = GT::child_begin(N), CE = GT::child_end(N); CI != CE; ++CI) {
-    auto VI = VisitedNodes.find(*CI);
-    if (VI == VisitedNodes.end()) {
-      std::pair<NodeType *, NodeType *> BE =
-        findBackEdge<GraphType>(*CI, VisitedNodes);
+    // The new node is inserted in VisitedNodes before a recursive call of
+    // the findBackEdge() function to reduce number of searches.
+    auto VI = VisitedNodes.insert(std::make_pair(*CI, COLOR_GRAY));
+    if (VI.second) {
+      auto BE = findBackEdge<GraphType>(*CI, VisitedNodes);
       if (BE.first)
         return BE;
-    } else if (VI->second == COLOR_GRAY) {
+      // The color of the node becomes black only if the back edge is not found,
+      // otherwise there is no assurance that all children have been opened.
+      // The new search is neccessary because the iterators in a DenseMap
+      // are invalidated  whenever an insertion occurs.
+      VisitedNodes.find(*CI)->second = COLOR_BLACK;
+    } else if (VI.first->second == COLOR_GRAY) {
       return std::make_pair(N, *CI);
     }
-    VI->second = COLOR_BLACK;
   }
   return std::pair<NodeType *, NodeType *>(nullptr, nullptr);
+}
+}
+
+/// \brief Returns first back edge in the specified directed graph.
+///
+/// This function implements a preorder traversal using DFS and calculates
+/// the nearest back edge to the graph entry node.
+/// \param [in] G Directed graph, it can not be null.
+/// \return Back edge represented as a pair of two nodes: successor and
+/// predecessor.
+/// If back edge in not found this function returns a pair of nullptrs
+/// \pre The llvm::GraphTraits class should be specialized by GraphType.
+/// Note that GraphType is generally a pointer type, for example BasicBlock *.
+template<class GraphType>
+inline std::pair<
+  typename llvm::GraphTraits<GraphType>::NodeType *,
+  typename llvm::GraphTraits<GraphType>::NodeType *> findBackEdge(GraphType G) {
+  typedef llvm::GraphTraits<GraphType > GT;
+  typedef typename GT::NodeType NodeType;
+  assert(G && "Graph must not be null!");
+  llvm::DenseMap<NodeType *, DFSColor> VisitedNodes;
+  VisitedNodes.insert(std::make_pair(GT::getEntryNode(G), COLOR_GRAY));
+  return detail::findBackEdge<GraphType>(GT::getEntryNode(G), VisitedNodes);
 }
 
 /// \brief Returns true if the specified directed graph is acyclic.
 ///
 /// \param [in] G Directed graph, it can not be null.
-/// \pre The llvm::GraphTraits class should be specialized by GraphType *.
-template<class GraphType> bool isDAG(GraphType *G) {
-  typedef llvm::GraphTraits<GraphType *> GT;
-  typedef typename GT::NodeType NodeType;
-  assert(G && "Graph must not be null!");
-  llvm::DenseMap<NodeType *, DFSColor> VisitedNodes;
-  return !findBackEdge<GraphType>(GT::getEntryNode(G), VisitedNodes).first;
+/// \pre The llvm::GraphTraits class should be specialized by GraphType.
+/// Note that GraphType is generally a pointer type, for example BasicBlock *.
+template<class GraphType> inline bool isDAG(GraphType G) {
+  assert(G && "Graph must not be null!");  
+  return !findBackEdge<GraphType>(G).first;
 }
 }
 #endif//TSAR_GRAPH_H
