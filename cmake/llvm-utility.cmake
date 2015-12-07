b01ae715b3378fb55cf6ae0e4c6583436d73f70f
Kataev <Kataev@c90b6321-6eb1-4274-a4bb-7129d0cd3ae9>
2015-12-07 14:57:20 +0000

    [TSAR-cmake] Update scripts for CMake.
    It is possible to build Clang, opt and TSAR in the same solution and install them in the specified folder.
    Patches for LLVM and Clang have been added.
    There are two types of patches. The first one has the tsar prefix and is necessary to build TSAR. 
    The second one eliminates errors in LLVM and Clang.
    
    git-svn-id: http://svn.dvm-system.org/svn/dvmhrepo@3102 c90b6321-6eb1-4274-a4bb-7129d0cd3ae9

--- cmake/llvm-utility.cmake
+++ cmake/llvm-utility.cmake
@@ -1,20 +1,36 @@
 # This download LLVM sources from repository.
 # Pre: subversion package must be available, ${LLVM_SOURCE_DIR} and ${LLVM_REPO} must be set.
+#      If BUILD_CLANG is set then ${CLANG_SOURCE_DIR} and ${CLANG_REPOS} must be also set.
 # Post: check out a working copy ${LLVM_SOURCE_DIR} from LLVM repository ${LLVM_REPO} or
 #       update it if the working copy already exist.
+#       If BUILD_CLANG is set check out a working copy ${CLANG_SOURCE_DIR}
+#       from Clang repository ${CLANG_REPO} or update it if the working copy already exist.
 function(sapfor_download_llvm)
     if(NOT EXISTS ${LLVM_SOURCE_DIR})
         set(LLVM_STATUS "Checking out LLVM working copy")
         message(STATUS ${LLVM_STATUS})
         execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} co ${LLVM_REPO} ${LLVM_SOURCE_DIR}
                                 RESULT_VARIABLE LLVM_ERROR)
+        if(BUILD_CLANG AND NOT LLVM_ERROR)
+            message(STATUS "${LLVM_STATUS} - done")
+            set(LLVM_STATUS "Checking out Clang working copy")
+            message(STATUS ${LLVM_STATUS})
+            execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} co ${CLANG_REPO} 
+                                    ${CLANG_SOURCE_DIR} RESULT_VARIABLE LLVM_ERROR)
+        endif()
     else()
         set(LLVM_STATUS "Updating LLVM working copy")
         message(STATUS ${LLVM_STATUS})
-            execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} up ${LLVM_SOURCE_DIR}
+        execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} up ${LLVM_SOURCE_DIR}
+                                RESULT_VARIABLE LLVM_ERROR)
+        if(BUILD_CLANG AND NOT LLVM_ERROR)
+            message(STATUS "${LLVM_STATUS} - done")
+            set(LLVM_STATUS "Updating Clang working copy")
+            message(STATUS ${LLVM_STATUS})
+            execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} up ${CLANG_SOURCE_DIR}
                                     RESULT_VARIABLE LLVM_ERROR)
-    endif()
-
+        endif()
+    endif()    
     if(LLVM_ERROR)
        message(FATAL_ERROR "${LLVM_STATUS} - error")
     else()
--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -4,17 +4,17 @@
 # If the option already is on the variable, don't add it.
 # This code is taken from LLVM (cmake/modules/LLVMProcessSources.cmake).
 function(sapfor_replace_compiler_option var old new)
-    if("${${var}}" MATCHES "(^| )${new}($| )")
-        set(n "")
-    else()
-        set(n "${new}")
-    endif()
-        if("${${var}}" MATCHES "(^| )${old}($| )")
-        string(REGEX REPLACE "(^| )${old}($| )" " ${n} " ${var} "${${var}}")
-    else()
-        set(${var} "${${var}} ${n}")
-    endif()
-    set( ${var} "${${var}}" PARENT_SCOPE )
+  if("${${var}}" MATCHES "(^| )${new}($| )")
+    set(n "")
+  else()
+    set(n "${new}")
+  endif()
+  if("${${var}}" MATCHES "(^| )${old}($| )")
+    string(REGEX REPLACE "(^| )${old}($| )" " ${n} " ${var} "${${var}}")
+  else()
+    set(${var} "${${var}} ${n}")
+  endif()
+  set( ${var} "${${var}}" PARENT_SCOPE )
 endfunction(sapfor_replace_compiler_option)
 
 # Appends 'value' to a list of specified variables.
@@ -25,3 +25,29 @@ function(append value)
     set(${variable} "${${variable}} ${value}" PARENT_SCOPE)
   endforeach(variable)
 endfunction()
+
+# Applaies a patch 'patch' to a working in a 'source_dir'
+# The optional third parameter specifies list of options for svn patch command.
+# Pre: subversion package must be available.
+# Post: if 'patch' and 'source_dir' exists than the specified patch 
+# will be applied.
+function(sapfor_patch patch source_dir)
+  if(NOT EXISTS ${patch} OR NOT EXISTS ${source_dir})
+    return()
+  endif()  
+  set(status "Applying patch ${patch} to ${source_dir}")
+  if(${ARGC} LESS 3)
+    set(options "")
+  else()
+    set(options ${ARGV2})
+    set(status "${status} with options '${options}'")
+  endif()
+  message(STATUS ${status})    
+  execute_process(COMMAND ${Subversion_SVN_EXECUTABLE} 
+    patch ${options} ${patch} ${source_dir} RESULT_VARIABLE error)
+  if(error)
+    message(FATAL_ERROR "${status} - error")
+  else()       
+    message(STATUS "${status} - done")
+  endif()
+endfunction(sapfor_patch)
