9635cae8eca8a5976c815d654e8447359aa98006
Nikita Kataev <kaniandr@gmail.com>
2017-11-17 13:53:05 +0300

    [TSAR, GUI] Move handling of requests into a separate functions, update FunctionList handler.

--- src/PrivateServerPass.cpp
+++ src/PrivateServerPass.cpp
@@ -36,6 +36,8 @@
 #include <bcl/IntrusiveConnection.h>
 #include <bcl/Json.h>
 #include <bcl/RedirectIO.h>
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/Stmt.h>
 #include <llvm/ADT/StringSwitch.h>
 #include <llvm/Analysis/Passes.h>
 #include <llvm/Analysis/BasicAliasAnalysis.h>
@@ -72,7 +74,7 @@ JSON_OBJECT_ROOT_PAIR_5(Statistic,
   Statistic() : JSON_INIT_ROOT, JSON_INIT(Statistic, 0) {
     (*this)[Statistic::Traits].for_each(InitTraitsFunctor());
   }
-  ~Statistic() = default;
+  ~Statistic() override {}
 
   Statistic(const Statistic &) = default;
   Statistic & operator=(const Statistic &) = default;
@@ -81,11 +83,12 @@ JSON_OBJECT_ROOT_PAIR_5(Statistic,
 JSON_OBJECT_END(Statistic)
 
 JSON_OBJECT_BEGIN(MainLoopInfo)
-JSON_OBJECT_PAIR_4(MainLoopInfo,
+JSON_OBJECT_PAIR_5(MainLoopInfo,
   StartCol, unsigned,
   StartLine, unsigned,
   EndCol, unsigned,
-  EndLine, unsigned)
+  EndLine, unsigned,
+  Level, unsigned)
 
   MainLoopInfo() = default;
   ~MainLoopInfo() = default;
@@ -96,14 +99,26 @@ JSON_OBJECT_PAIR_4(MainLoopInfo,
   MainLoopInfo & operator=(MainLoopInfo &&) = default;
 JSON_OBJECT_END(MainLoopInfo)
 
-JSON_OBJECT_BEGIN(FunctionList)
-JSON_OBJECT_ROOT_PAIR_3(FunctionList,
-  Functions, std::vector<std::string>,
-  NumLoops, std::vector<unsigned>,
+JSON_OBJECT_BEGIN(MainFuncInfo)
+JSON_OBJECT_PAIR_2(MainFuncInfo,
+  Name, std::string,
   Loops, std::vector<MainLoopInfo>)
 
+  MainFuncInfo() = default;
+  ~MainFuncInfo() = default;
+
+  MainFuncInfo(const MainFuncInfo &) = default;
+  MainFuncInfo & operator=(const MainFuncInfo &) = default;
+  MainFuncInfo(MainFuncInfo &&) = default;
+  MainFuncInfo & operator=(MainFuncInfo &&) = default;
+JSON_OBJECT_END(MainFuncInfo)
+
+JSON_OBJECT_BEGIN(FunctionList)
+JSON_OBJECT_ROOT_PAIR(FunctionList,
+  Functions, std::vector<MainFuncInfo>)
+
   FunctionList() : JSON_INIT_ROOT {}
-  ~FunctionList() = default;
+  ~FunctionList() override {}
 
   FunctionList(const FunctionList &) = default;
   FunctionList & operator=(const FunctionList &) = default;
@@ -115,6 +130,7 @@ JSON_OBJECT_END(FunctionList)
 
 JSON_DEFAULT_TRAITS(tsar::msg::, Statistic)
 JSON_DEFAULT_TRAITS(tsar::msg::, MainLoopInfo)
+JSON_DEFAULT_TRAITS(tsar::msg::, MainFuncInfo)
 JSON_DEFAULT_TRAITS(tsar::msg::, FunctionList)
 
 using ServerPrivateProvider = FunctionPassProvider<
@@ -181,6 +197,114 @@ void incrementTraitCount(ServerPrivateProvider &P, TraitMap &TM) {
 }
 }
 
+std::string answerStatistic(llvm::PrivateServerPass * const PSP,
+    llvm::Module &M, tsar::TransformationContext *TfmCtx) {
+  msg::Statistic Stat;
+  auto &Rewriter = TfmCtx->getRewriter();
+  for (auto FI = Rewriter.getSourceMgr().fileinfo_begin(),
+    EI = Rewriter.getSourceMgr().fileinfo_end(); FI != EI; ++FI) {
+    auto Ext = sys::path::extension(FI->first->getName());
+    std::string Kind;
+    if (std::find(
+      msg::HeaderFile::extensions().begin(),
+      msg::HeaderFile::extensions().end(), Ext) !=
+      msg::HeaderFile::extensions().end())
+      Kind = msg::HeaderFile::name();
+    else if (std::find(
+      msg::SourceFile::extensions().begin(),
+      msg::SourceFile::extensions().end(), Ext) !=
+      msg::SourceFile::extensions().end())
+      Kind = msg::SourceFile::name();
+    else
+      Kind = msg::OtherFile::name();
+    auto I = Stat[msg::Statistic::Files].insert(std::make_pair(Kind, 1));
+    if (!I.second)
+      ++I.first->second;
+  }
+  auto &MMP = PSP->getAnalysis<MemoryMatcherImmutableWrapper>();
+  Stat[msg::Statistic::Variables].insert(
+    std::make_pair(msg::Analysis::Yes, MMP->Matcher.size()));
+  Stat[msg::Statistic::Variables].insert(
+    std::make_pair(msg::Analysis::No, MMP->UnmatchedAST.size()));
+  std::pair<unsigned, unsigned> Loops(0, 0);
+  for (Function &F : M) {
+    if (F.empty())
+      continue;
+    ++Stat[msg::Statistic::Functions];
+    auto &Provider = PSP->getAnalysis<ServerPrivateProvider>(F);
+    auto &LMP = Provider.get<LoopMatcherPass>();
+    Loops.first += LMP.getMatcher().size();
+    Loops.second += LMP.getUnmatchedAST().size();
+    incrementTraitCount(Provider, Stat[msg::Statistic::Traits]);
+  }
+  Stat[msg::Statistic::Loops].insert(
+    std::make_pair(msg::Analysis::Yes, Loops.first));
+  Stat[msg::Statistic::Loops].insert(
+    std::make_pair(msg::Analysis::No, Loops.second));
+  return json::Parser<msg::Statistic>::unparseAsObject(Stat);
+}
+
+void getLoopInfo(clang::Stmt *Ptr, clang::SourceManager &SrcMgr,
+    msg::MainLoopInfo &Loop) {
+  auto LocStart = Ptr->getLocStart();
+  auto LocEnd = Ptr->getLocEnd();
+  auto StartCol = SrcMgr.getExpansionColumnNumber(LocStart);
+  auto StartLine = SrcMgr.getExpansionLineNumber(LocStart);
+  auto EndCol = SrcMgr.getExpansionColumnNumber(LocEnd);
+  auto EndLine = SrcMgr.getExpansionLineNumber(LocEnd);
+  Loop[msg::MainLoopInfo::StartCol] = StartCol;
+  Loop[msg::MainLoopInfo::StartLine] = StartLine;
+  Loop[msg::MainLoopInfo::EndCol] = EndCol;
+  Loop[msg::MainLoopInfo::EndLine] = EndLine;
+}
+
+std::string answerFunctionList(llvm::PrivateServerPass * const PSP,
+    llvm::Module &M, tsar::TransformationContext *TfmCtx) {
+  msg::FunctionList FuncLst;
+  typedef msg::MainFuncInfo FuncInfo;
+  typedef msg::MainLoopInfo LoopInfo;
+  auto &SrcMgr = TfmCtx->getContext().getSourceManager();
+  for (Function &F : M) {
+    if (F.empty())
+      continue;
+    msg::MainFuncInfo Func;
+    Func[FuncInfo::Name] = F.getName();
+    auto &Provider = PSP->getAnalysis<ServerPrivateProvider>(F);
+    auto &Matcher = Provider.get<LoopMatcherPass>().getMatcher();
+    auto &Unmatcher = Provider.get<LoopMatcherPass>().getUnmatchedAST();
+    msg::MainLoopInfo Loop;
+    for (auto &Match : Matcher) {
+      getLoopInfo(Match.get<AST>(), SrcMgr, Loop);
+      Func[FuncInfo::Loops].push_back(Loop);
+    }
+    for (auto &Unmatch : Unmatcher) {
+      getLoopInfo(Unmatch, SrcMgr, Loop);
+      Func[FuncInfo::Loops].push_back(Loop);
+    }
+    std::sort(Func[FuncInfo::Loops].begin(),
+      Func[FuncInfo::Loops].end(),
+      [](msg::MainLoopInfo &LHS,
+          msg::MainLoopInfo &RHS) -> bool {
+      return (LHS[LoopInfo::StartLine] < RHS[LoopInfo::StartLine]) ||
+          ((LHS[LoopInfo::StartLine] == RHS[LoopInfo::StartLine]) &&
+          (LHS[LoopInfo::StartCol] < RHS[LoopInfo::StartCol]));
+    });
+    std::vector<std::pair<unsigned, unsigned>> Levels;
+    for (auto &Loop : Func[FuncInfo::Loops]) {
+      while (!Levels.empty() &&
+          ((Levels[Levels.size() - 1].first < Loop[LoopInfo::EndLine]) ||
+          ((Levels[Levels.size() - 1].first == Loop[LoopInfo::EndLine]) &&
+          (Levels[Levels.size() - 1].second < Loop[LoopInfo::EndCol]))))
+        Levels.pop_back();
+      Loop[msg::MainLoopInfo::Level] = Levels.size() + 1;
+      Levels.emplace_back(
+        std::make_pair(Loop[LoopInfo::EndLine], Loop[LoopInfo::EndCol]));
+    }
+    FuncLst[msg::FunctionList::Functions].push_back(Func);
+  }
+  return json::Parser<msg::FunctionList>::unparseAsObject(FuncLst);
+}
+
 bool PrivateServerPass::runOnModule(llvm::Module &M) {
   if (!mConnection) {
     errs() << "error: intrusive connection is not specified for the module "
@@ -197,86 +321,21 @@ bool PrivateServerPass::runOnModule(llvm::Module &M) {
     [&M, &TfmCtx](TransformationEnginePass &TEP) {
       TEP.setContext(M, TfmCtx);
   });
-  mConnection->answer(
+  while (mConnection->answer(
       [this, &M, &TfmCtx](const std::string &Request) -> std::string {
     msg::Diagnostic Diag(msg::Status::Error);
     if (mStdErr->isDiff()) {
       Diag[msg::Diagnostic::Terminal] += mStdErr->diff();
       return json::Parser<msg::Diagnostic>::unparseAsObject(Diag);
     }
-    msg::Statistic Stat;
-    json::Parser<> P(Request);
-    if (!P.parse(Stat)) {
-      Diag.insert(msg::Diagnostic::Error, P.errors());
-      return json::Parser<msg::Diagnostic>::unparseAsObject(Diag);
-    }
-    auto &Rewriter = TfmCtx->getRewriter();
-    for (auto FI = Rewriter.getSourceMgr().fileinfo_begin(),
-         EI = Rewriter.getSourceMgr().fileinfo_end(); FI != EI; ++FI) {
-      auto Ext = sys::path::extension(FI->first->getName());
-      std::string Kind;
-      if (std::find(
-            msg::HeaderFile::extensions().begin(),
-            msg::HeaderFile::extensions().end(), Ext) !=
-          msg::HeaderFile::extensions().end())
-        Kind = msg::HeaderFile::name();
-      else if (std::find(
-            msg::SourceFile::extensions().begin(),
-            msg::SourceFile::extensions().end(), Ext) !=
-          msg::SourceFile::extensions().end())
-        Kind = msg::SourceFile::name();
-      else
-        Kind = msg::OtherFile::name();
-      auto I = Stat[msg::Statistic::Files].insert(std::make_pair(Kind, 1));
-      if (!I.second)
-        ++I.first->second;
-    }
-    auto &MMP = getAnalysis<MemoryMatcherImmutableWrapper>();
-    Stat[msg::Statistic::Variables].insert(
-      std::make_pair(msg::Analysis::Yes, MMP->Matcher.size()));
-    Stat[msg::Statistic::Variables].insert(
-      std::make_pair(msg::Analysis::No, MMP->UnmatchedAST.size()));
-    std::pair<unsigned, unsigned> Loops(0, 0);
-    for (Function &F : M) {
-      if (F.empty())
-        continue;
-      ++Stat[msg::Statistic::Functions];
-      auto &Provider = getAnalysis<ServerPrivateProvider>(F);
-      auto &LMP = Provider.get<LoopMatcherPass>();
-      Loops.first += LMP.getMatcher().size();
-      Loops.second += LMP.getUnmatchedAST().size();
-      incrementTraitCount(Provider, Stat[msg::Statistic::Traits]);
-    }
-    Stat[msg::Statistic::Loops].insert(
-      std::make_pair(msg::Analysis::Yes, Loops.first));
-    Stat[msg::Statistic::Loops].insert(
-      std::make_pair(msg::Analysis::No, Loops.second));
-    return json::Parser<msg::Statistic>::unparseAsObject(Stat);
-  });
-  mConnection->answer(
-      [this, &M](const std::string &Request) -> std::string {
-    msg::FunctionList FuncLst;
-    for (Function &F : M) {
-      if (F.empty())
-        continue;
-      FuncLst[msg::FunctionList::Functions].push_back(F.getName());
-      unsigned NL = 0;
-      auto &Provider = getAnalysis<ServerPrivateProvider>(F);
-      auto &Matcher = Provider.get<LoopMatcherPass>().getMatcher();
-      for (auto &Match : Matcher) {
-        NL++;
-        auto LR = Match.get<IR>()->getLocRange();
-        msg::MainLoopInfo LoopInfo;
-        LoopInfo[msg::MainLoopInfo::StartCol] = LR.getStart().getCol();
-        LoopInfo[msg::MainLoopInfo::StartLine] = LR.getStart().getLine();
-        LoopInfo[msg::MainLoopInfo::EndCol] = LR.getEnd().getCol();
-        LoopInfo[msg::MainLoopInfo::EndLine] = LR.getEnd().getLine();
-        FuncLst[msg::FunctionList::Loops].push_back(LoopInfo);
-      }
-      FuncLst[msg::FunctionList::NumLoops].push_back(NL);
-    }
-    return json::Parser<msg::FunctionList>::unparseAsObject(FuncLst);
-  });
+    json::Parser<msg::Statistic, msg::FunctionList> P(Request);
+    auto Obj = P.parse();
+    assert(Obj && "error: invalid request");
+    if (Obj->is<msg::Statistic>())
+      return answerStatistic(this, M, TfmCtx);
+    if (Obj->is<msg::FunctionList>())
+      return answerFunctionList(this, M, TfmCtx);
+  }));
   return false;
 }
 
