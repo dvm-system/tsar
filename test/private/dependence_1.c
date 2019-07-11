9d5c52486093c5386604978f02ad68932628a7ee
Nikita Kataev <kaniandr@gmail.com>
2019-07-11 16:00:21 +0300

    [TSAR, Dep, Memory, Test] Add test.

---
+++ test/private/dependence_1.c
@@ -0,0 +1,26 @@
+void bar(double *A);
+
+void foo(int N) {
+  double A[N][10];
+  // Dependence may exist because bar() could add offset to its parameter
+  // and the same call may access different rows of A.
+  // Option '-finbounds-subscripts' has no sense in this case.
+  for (int I = 0; I < 10; ++I)
+    bar(A[I]);
+}
+//CHECK: Printing analysis 'Dependency Analysis (Metadata)' for function 'foo':
+//CHECK:  loop at depth 1 dependence_1.c:8:3
+//CHECK:    output:
+//CHECK:     <A:{4:3|4:10}, ?> | bar():9:5
+//CHECK:    anti:
+//CHECK:     <A:{4:3|4:10}, ?> | bar():9:5
+//CHECK:    flow:
+//CHECK:     <A:{4:3|4:10}, ?> | bar():9:5
+//CHECK:    induction:
+//CHECK:     <I:8:12, 4>:[Int,0,10,1]
+//CHECK:    lock:
+//CHECK:     <I:8:12, 4>
+//CHECK:    header access:
+//CHECK:     <A:{4:3|4:10}, ?> | <I:8:12, 4> | bar():9:5
+//CHECK:    explicit access:
+//CHECK:     <I:8:12, 4> | bar():9:5
--- test/private/shared_8.c
+++ test/private/shared_8.c
@@ -6,31 +6,31 @@ void foo(int N, int JStart, double (*U)[N]) {
       U[I][J] = U[I][J] + 1;
 }
 //CHECK: Printing analysis 'Dependency Analysis (Metadata)' for function 'foo':
-//CHECK:  loop at depth 1 shared_7.c:5:3
+//CHECK:  loop at depth 1 shared_8.c:4:3
 //CHECK:    shared:
-//CHECK:     <U, 80000>
+//CHECK:     <*U:3:38, ?> <IEnd, 4> <JEnd, 4>
 //CHECK:    private:
-//CHECK:     <J:6:14, 4>
+//CHECK:     <J:5:14, 4>
 //CHECK:    induction:
-//CHECK:     <I:5:12, 4>:[Int,,,1]
+//CHECK:     <I:4:12, 4>:[Int,,,1]
 //CHECK:    read only:
-//CHECK:     <IEnd, 4> | <JEnd, 4> | <JStart:4:14, 4>
+//CHECK:     <JStart:3:21, 4> | <U:3:38, 8>
 //CHECK:    lock:
-//CHECK:     <I:5:12, 4> | <IEnd, 4>
+//CHECK:     <I:4:12, 4>
 //CHECK:    header access:
-//CHECK:     <I:5:12, 4> | <IEnd, 4> | <JEnd, 4>
+//CHECK:     <*U:3:38, ?> <IEnd, 4> <JEnd, 4> | <I:4:12, 4>
 //CHECK:    explicit access:
-//CHECK:     <I:5:12, 4> | <IEnd, 4> | <J:6:14, 4> | <JEnd, 4> | <JStart:4:14, 4>
-//CHECK:   loop at depth 2 shared_7.c:6:5
+//CHECK:     <I:4:12, 4> | <J:5:14, 4> | <JStart:3:21, 4> | <U:3:38, 8>
+//CHECK:   loop at depth 2 shared_8.c:5:5
 //CHECK:     shared:
-//CHECK:      <U, 80000>
+//CHECK:      <*U:3:38, ?> <JEnd, 4>
 //CHECK:     induction:
-//CHECK:      <J:6:14, 4>:[Int,,,1]
+//CHECK:      <J:5:14, 4>:[Int,,,1]
 //CHECK:     read only:
-//CHECK:      <I:5:12, 4> | <JEnd, 4>
+//CHECK:      <I:4:12, 4> | <U:3:38, 8>
 //CHECK:     lock:
-//CHECK:      <J:6:14, 4> | <JEnd, 4>
+//CHECK:      <J:5:14, 4>
 //CHECK:     header access:
-//CHECK:      <J:6:14, 4> | <JEnd, 4> | <U, 80000>
+//CHECK:      <*U:3:38, ?> <JEnd, 4> | <J:5:14, 4>
 //CHECK:     explicit access:
-//CHECK:      <I:5:12, 4> | <J:6:14, 4> | <JEnd, 4>
+//CHECK:      <I:4:12, 4> | <J:5:14, 4> | <U:3:38, 8>
