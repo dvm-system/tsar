f65032e3eb0f80bb7494ebd417e7deef039c265d
Nikita Kataev <kaniandr@gmail.com>
2019-08-01 13:48:55 +0300

    [TSAR, Dep, Memory, Metadata] Add test.

---
+++ test/private/distance_8.c
@@ -0,0 +1,53 @@
+const int N = 100;
+
+double A[N][N];
+
+void foo() {
+  int I, J;
+  for (I = 1; I < N; ++I) {
+    for (J = 0; J < N; ++J)
+      A[I][J] = A[I][J] + I + J;
+    for (J = 1; J < N; ++J)
+      A[I][J] = A[I-1][J] + A[I][J-1];
+  }
+}
+//CHECK: Printing analysis 'Dependency Analysis (Metadata)' for function 'foo':
+//CHECK:  loop at depth 1 distance_8.c:7:3
+//CHECK:    private:
+//CHECK:     <J:6:10, 4>
+//CHECK:    flow:
+//CHECK:     <A, 80000>:[1,1]
+//CHECK:    induction:
+//CHECK:     <I:6:7, 4>:[Int,1,100,1]
+//CHECK:    lock:
+//CHECK:     <I:6:7, 4>
+//CHECK:    header access:
+//CHECK:     <I:6:7, 4>
+//CHECK:    explicit access:
+//CHECK:     <I:6:7, 4> | <J:6:10, 4>
+//CHECK:   loop at depth 2 distance_8.c:10:5
+//CHECK:     flow:
+//CHECK:      <A, 80000>:[1,1]
+//CHECK:     induction:
+//CHECK:      <J:6:10, 4>:[Int,1,100,1]
+//CHECK:     read only:
+//CHECK:      <I:6:7, 4>
+//CHECK:     lock:
+//CHECK:      <J:6:10, 4>
+//CHECK:     header access:
+//CHECK:      <A, 80000> | <J:6:10, 4>
+//CHECK:     explicit access:
+//CHECK:      <I:6:7, 4> | <J:6:10, 4>
+//CHECK:   loop at depth 2 distance_8.c:8:5
+//CHECK:     shared:
+//CHECK:      <A, 80000>
+//CHECK:     induction:
+//CHECK:      <J:6:10, 4>:[Int,0,100,1]
+//CHECK:     read only:
+//CHECK:      <I:6:7, 4>
+//CHECK:     lock:
+//CHECK:      <J:6:10, 4>
+//CHECK:     header access:
+//CHECK:      <A, 80000> | <J:6:10, 4>
+//CHECK:     explicit access:
+//CHECK:      <I:6:7, 4> | <J:6:10, 4>
